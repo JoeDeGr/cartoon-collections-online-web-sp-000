02b058f1bd983c2f5aea9528a880c94e1b0bbd99
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-02-11 17:33:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,6 +14,15 @@ def long_planeteer_calls(calls)# code an argument here
 end
 
 def find_the_cheese(string)
+<<<<<<< HEAD
+=======
+  i = 0
+  while i < string.length do
+    return(string[i]) if cheese_type.any? {|cheese| cheese == string[i]}# code an argument here
+    i +=1
+  end
+  # the array below is here to help
+>>>>>>> 13433eb8fb784f30161f4c689c60d8c4f05de2c7
   cheese_types = ["cheddar", "gouda", "camembert"]
   i = 0
   while i < string.length do
